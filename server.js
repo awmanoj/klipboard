b6994c62a85670e3cf776a8c6764916c4c5eea0f
Manoj Awasthi <awasthi.manoj@gmail.com>
2022-07-26 20:33:46 +0700

    Add configurations

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express()
+
+config = require('./server/configure');
+app = config(app);
+
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
+
 app.get('/', function(req, res) {
 	res.send('Work in progress');
 });
+
 app.listen(app.get('port'), function() {
 	console.log('Server running: http://localhost:' + app.get('port'));
 });
